f1ccbb6623ac05a60c7e45a4e701d075875f3628
wuye <107172097+wuyeshen1998@users.noreply.github.com>
2023-06-23 17:54:18 +0100

    Update pinata.js

--- src/pinata.js
+++ src/pinata.js
@@ -1,7 +1,7 @@
 //require('dotenv').config();
 const key = process.env.REACT_APP_PINATA_KEY;
 const secret = process.env.REACT_APP_PINATA_SECRET;
-
+===================================================
 const axios = require('axios');
 const FormData = require('form-data');
 
